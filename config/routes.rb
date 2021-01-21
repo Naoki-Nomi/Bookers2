923c409a13a1bb568d4263c443766a63131cff83
Naoki-Nomi <ec2-user@ip-172-31-34-154.ec2.internal>
2021-01-21 13:05:03 +0000

    [6割完成]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  resources :books, except: [:new]
+  resources :users, only: [:show, :index, :edit, :update]
+
 end
